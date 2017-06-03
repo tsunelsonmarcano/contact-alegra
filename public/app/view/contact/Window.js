469461ee9340fb369edb1e94554f78bce35aea79
Nelson Marcano <tsu.nelsonmarcano@gmail.com>
2017-06-03 02:22:18 -0400

    Mejora visual en el modal

--- public/app/view/contact/Window.js
+++ public/app/view/contact/Window.js
@@ -2,8 +2,8 @@ Ext.define('CrudExt.view.contact.Window', {
 
 	extend: 'Ext.window.Window',
 	title: 'Editando registro',
-	width: '100%',
-	height: '100%',
+	width: '80%',
+	height: '90%',
 	layout: 'fit',
 	autoShow: true,
 	modal: true,
